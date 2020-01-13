c615b3d049bebdae61ab108162ae0013948ec3c6
Amrit Singh <asingh1092@gmail.com>
2020-01-12 23:04:30 -0500

    pushing all in

---
+++ src/main/java/com/singh/springdemo/config/SpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,29 @@
+package com.singh.springdemo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
+
+
+
+
+
+
